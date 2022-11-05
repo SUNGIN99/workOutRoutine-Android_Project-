c7ffb95690cf3a07875c9d977059755fd74c70d0
SUNGIN99 <@tjddls1920>
2022-11-06 00:39:20 +0900

    22.11.05 리싸이클러뷰 공부하기.. 아직까지 잘 모르겟음

--- app/src/main/java/com/example/workoutroutine/MainActivity.java
+++ app/src/main/java/com/example/workoutroutine/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.workoutroutine;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    RecyclerView recyclerView;
+    Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        //상하 스크롤
+
+        adapter = new Adapter();
+        for(int i = 0; i< 100; i++){
+            String str= i+"번째 아이템";
+            adapter.setArrayData(str);
+        }
+
+        recyclerView.setAdapter(adapter);
     }
+
 }
