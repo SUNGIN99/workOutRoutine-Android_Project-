e37e8f5c838afe5d2ac544d713ab90bd3259dcae
SUNGIN99 <@tjddls1920>
2022-11-15 22:03:17 +0900

    22.11.15
    RoomDB실습
    
    + recyclerView
    
    1) insert, update, delete
    2) interface dao
    3) adapter
    4) roomDB

---
+++ app/src/main/java/com/example/workoutroutine/MainData.java
@@ -0,0 +1,33 @@
+package com.example.workoutroutine;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+
+@Entity(tableName = "table_name")
+public class MainData implements Serializable {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name="text")
+    private String text;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
